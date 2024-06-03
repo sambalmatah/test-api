d4078ac1d943a97d6fbbd31b23204a866cd4aa1b
sambalmatah <yudiarinugroho14@gmail.com>
2024-06-03 14:55:55 +0700

    push port

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { connection } = require('./db');
 const app = express()
 
+const port = process.env.MYSQL_PORT || 8080
+
 app.get('/api/shopping-list', (req, res) => {
     connection.query('SELECT * FROM shopping_list', (err, data) => {
         if (err) return callback(err, null);
@@ -13,8 +15,8 @@ app.get('/api/shopping-list', (req, res) => {
     });
 });
 
-app.listen(8080,
+app.listen(port,
     () => {
-        console.log(`Server is listening on port ${8080}`)
+        console.log(`Server is listening on port ${port}`)
     }
 )
